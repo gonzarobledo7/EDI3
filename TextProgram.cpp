28f4516a0714ebb418686024c3f39f07907507b9
gonzarobledo7 <50006048+gonzarobledo7@users.noreply.github.com>
2020-04-09 00:26:01 -0300

    Add files via upload
    Profe le vuelvo a pasar el Repositorio con el cpp diagnostico que nos habia pedido.

---
+++ TextProgram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout << "Programa Diagnostico EDI 3/2020" << "\n" << "Ingrese texto: " << "\n";
+
+    string texto;
+    cin >> texto;
+
+    cout << "Usted ingreso el siguiente texto: " << "\n" <<texto;
+
+}
+
+
